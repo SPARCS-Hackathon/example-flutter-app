2280d586fc9d0a420ef59065cb1f0e77b6c8ad30
Jiho Park <jihopark7777@gmail.com>
2023-01-29 00:59:19 +0900

    :tada: Create Flutter App

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then((value) => runApp(MyApp()));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    // 1 <-- SEE HERE
+    return CupertinoApp(
+      // 2 <-- SEE HERE
+      theme: CupertinoThemeData(brightness: Brightness.light),
+      home: CupertinoSimpleHomePage(),
+    );
+  }
+}
+
+class CupertinoSimpleHomePage extends StatefulWidget {
+  const CupertinoSimpleHomePage({Key? key}) : super(key: key);
+
+  @override
+  _CupertinoSimpleHomePageState createState() =>
+      _CupertinoSimpleHomePageState();
+}
+
+class _CupertinoSimpleHomePageState extends State<CupertinoSimpleHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // 3 <-- SEE HERE
+    return const CupertinoPageScaffold(
+      // 4 <-- SEE HERE
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('SPARCS 2023 StartUp Hackathon'),
+      ),
+      child: Center(
+        child: Text('If you run this app, you must participate in hackathon!'),
+      ),
+    );
+  }
+}
